cc98e087529b14f5233c4eabd408ef97ea683b9c
phil olson <philip.olson@protonmail.ch>
2016-10-28 04:04:33 -0500

    next release wuuuut

--- index.pack.js
+++ index.pack.js
@@ -145,7 +145,11 @@ module.exports =
 	const getRequestListener = (letsencryptDir, server, ioNamespace) => {
 	  io = socketIo(server).of(ioNamespace);
 
-	  if (letsencryptDir) app.use(express.static(letsencryptDir));
+	  if (letsencryptDir) {
+	    app.use(express.static(letsencryptDir, {
+	      dotfiles: 'allow'
+	    }));
+	  }
 
 	  app.engine('hbs', hbs.express4({ partials: 'views/partials' }))
 	    .set('view engine', 'hbs')
